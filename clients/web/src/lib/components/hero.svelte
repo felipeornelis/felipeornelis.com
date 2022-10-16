645280fcefdd51f8c6195b685a7a998616b7aaa8
Felipe Ornelis <ola@felipeornelis.com>
2022-10-16 01:51:59 -0300

    refactor(hero.svelte): rewrite hero.svelte code and create icons components

--- clients/web/src/lib/components/hero.svelte
+++ clients/web/src/lib/components/hero.svelte
@@ -1,15 +1,33 @@
 <script lang="ts">
+	import Svelte from '$lib/icons/svelte.svelte';
+	import Go from '$lib/icons/go.svelte';
+	import TypeScript from '$lib/icons/typescript.svelte';
+
+	const colors = {
+		svelte: '#ff3e00',
+		go: '#007d9c',
+		typescript: '#0076c6'
+	};
 </script>
 
-<div class="h-[700px] w-full pt-24">
-	<div>
-		<p class="font-bold text-6xl text-white tracking-tighter">
-			I'm Felipe <span class="bg-hero-blue bg-clip-text text-transparent">Ornelis</span>
-		</p>
-		<div class="text-xl text-gray-600">
-			<p>Fullstack Developer</p>
-			<p>Sharing knowledge about Web Development,</p>
-			<p>Developer Experience and Test Engineering</p>
+<section class="w-full">
+	<div class="grid grid-cols-2 pt-32">
+		<div>
+			<div class="flex flex-col gap-3">
+				<p class="font-bold text-6xl text-white tracking-tighter">
+					I'm Felipe <span class="bg-hero-blue bg-clip-text text-transparent">Ornelis</span>
+				</p>
+				<div class="text-xl text-gray-600">
+					<p class="text-white">Fullstack Developer and Quality Assurance Engineer</p>
+					<p class="mt-10">Sharing knowledge about Web Development,</p>
+					<p>Developer Experience and Test Engineering</p>
+				</div>
+			</div>
+		</div>
+		<div class="flex items-center justify-center">
+			<Svelte size={7} color={colors.svelte} />
+			<Go size={7} color={colors.go} />
+			<TypeScript size={7} color={colors.typescript} />
 		</div>
 	</div>
-</div>
+</section>
