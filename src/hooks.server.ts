5217dc035f79d8f451b49e1d84f8966aeb432591
Felipe Ornelis <ola@felipeornelis.com>
2024-06-12 19:17:02 -0300

    chore: remove hooks.server.ts file

--- src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-import { PostRepositoryMemory } from './routes/sypave/+page.server';
-
-export const handle: Handle = ({ event, resolve }) => {
-	const repository = new PostRepositoryMemory();
-
-	return resolve(event);
-};
