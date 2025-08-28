db0768026cee39c720a6ad4d71127eaa27af9385
Felipe Ornelis <hello@felipeornelis.com>
2025-08-28 15:07:50 -0300

    feat: add button component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+	import { Button } from "$lib/components/button";
+	import { Head } from "$lib/components/head";
+
+    let metadata = {
+        title: 'Oieeeeee',
+    }
+
+</script>
+
+<Head {...metadata} />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
