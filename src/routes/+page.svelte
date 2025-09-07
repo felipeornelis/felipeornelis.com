52a3f9eb83e67068fb359e2a80afed4af87a5848
Felipe Ornelis <hello@felipeornelis.com>
2025-09-07 12:58:06 -0300

    feat: add props to AudioCard component
    Actually, it is poorly implemented, because I am not going to use it by now. Whenever needed, I come to finish it properly.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Head } from "$lib/components/head";
 	import Link from "$lib/components/link/link.svelte";
+	import { AudioCard } from "$lib/components/ui/audio-card";
 
     let metadata = {
         title: 'Oieeeeee',
@@ -12,4 +13,3 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <Link href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</Link> to read the documentation</p>
-
